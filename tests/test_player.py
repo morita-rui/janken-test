14196a60be13d7d71068f00991c5ba7bcbf3e450
moritarui <r.morita.sys24@morijyobi.ac.jp>
2024-12-12 11:48:08 +0900

    test-player#1

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,4 +1,28 @@
-import unittest
-from source.player import player
+import unittest;
+from unittest.mock import patch
+from source.player import player_pon;
 
-
+class Testplayer(unittest.TestCase):
+    @patch('builtins.input', return_value='1')
+    def test_player_pong(self, mock_input):
+            result = player_pon()
+            self.assertEqual(result, 'グー')
+    
+    @patch('builtins.input', return_value='2')
+    def test_player_pont(self, mock_input):
+            result = player_pon()
+            self.assertEqual(result, 'チョキ')
+            
+    @patch('builtins.input', return_value='3')
+    def test_player_pontp(self, mock_input):
+            result = player_pon()
+            self.assertEqual(result, 'パー')
+    
+    @patch('builtins.input', side_effect = ['0', '4', '1'])
+    def test_player_pon(self, mock_input):
+            result = player_pon()
+            self.assertEqual(result, 'グー')
+            
+            
+if __name__ == '__main__':
+    unittest.main()
